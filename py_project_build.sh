7020c447dcfb5b1133e38512ab4a69a4102e0818
anthonyjgatti <anthony.j.gatti@github.com>
2017-11-26 20:24:00 -0500

    Adding python_runner.sh

--- py_project_build.sh
+++ py_project_build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # A helper program for generating the folder 
 # structure for Python projects.
@@ -19,13 +19,21 @@ then
     exit 1
 fi
 
-
 # Make sure current directory is good
 
 
-#### Tracking Sphinx stuff here.
-# pip install Sphinx
+################################################################
+#### Tracking setup of virtualenv
+# pip install virtualenvwrapper
+# echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+# I also changed the location of $WORKON_HOME and $PROJECT_HOME
+# in my ~/.bashrc.
+# source ~/.bashrc
+################################################################
 
+# Create virtualenv.
+mkvirtualenv $1
+pip install sphinx sphinx-gallery
 
 # Generate base directory and important files.
 mkdir $1
@@ -48,10 +56,13 @@ $1/**/*.html
 
 EOM
 
+
+################## This part questionable. ####################################
 # Generate markdown docs directory. Because I like Markdown better,
 # I write docs in markdown, then use pandoc to convert them to .rst for Sphinx.
 # There are other solutions but this is my preferred.
 mkdir md_docs
+###############################################################################
 
 # Generate boilerplate contents of setup.py.
 cat > setup.py << EOM
@@ -60,58 +71,31 @@ from setuptools import setup, find_packages
 
 setup(
     name = '$1',
-    version = $1.__version__,
+    # version = $1.__version__,
     url = '',
     license = 'MIT',
     author = 'Anthony Gatti',
-    author_email = 'anthony.j.gatti@gmail.com'
+    author_email = 'anthony.j.gatti@gmail.com',
     tests_require = ['pytest'],
     install_requires = [],
     description = '',
-    long_description = long_description,
+    # long_description = long_description,
     packages = find_packages(),
-    test_suite = 'test.test_$1'
+    # test_suite = 'test.test_$1'
 )
-
-EOM
-
-# Generate boilerplate for conf.py for Sphinx docs.
-### GO BACK AND MAKE DATE AND VERSION DYNAMIC.
-cat > docs/conf.py << EOM
-# -*- coding: utf-8 -*-
-#
-# -- General configuration --
-
-souce_suffix = '.rst'
-master_doc = 'index'
-project = u'Insert project name here!'
-copyright = u'2017, Anthony Gatti'
-version = '0.0.1'
-
-# -- Options for HTML output --
-html_theme = "alabaster"
-html_theme_options = {
-    "rightsidebar": "true"
-}
 
 EOM
 
 # Create directories and set up main module.
 mkdir -p $1/tests
-cd ./$1
-touch __init__.py
-cd ..
-
-# Run sphinx-quickstart with configs.
-### FIGURE OUT DYNAMIC VERSIONING.
-#sphinx-quickstart -q -p $1 -a "Anthony Gatti" \
-#    -v 0.0.1 -l python --sep --makefile
+touch ./$1/__init__.py
+touch ./$1/tests/__init__.py
 
 # Create build script to be used to build project.
 cat > build.sh << EOM
 #!/usr/bin/env bash
 
-if [[ \$# -e 1 ]];
+if [[ \$# -ge 1 ]];
 then
     message="\$1";
 else
@@ -119,10 +103,20 @@ else
 fi
 
 # Convert files in md_docs to .rst, output in docs.
-find ./md_docs -iname "*.md" -type f -exec sh -c 'pandoc "\${1}" -o "./docs/\${0%.md}.rst"' {} \;
+# find ./md_docs -iname "*.md" -type f -exec sh -c 'pandoc "\${1}" -o "./docs/\${0%.md}.rst"' {} \;
 
-# Build the docs.
-sphinx-build -b html docs docs/generated
+# Start virtualenv.
+workon \$1
+# pip freeze > requirements.txt
+### DO LOOKUP HERE to replace == with >= in package versions.
+# pip install -r requirements.txt
+
+# Build the docs. Figure out versioning!
+sphinx-apidoc -F -A "Anthony Gatti" -o docs $1
+
+# Source python project.
+sudo python setup.py develop
+### CHANGE THIS PLEASE.
 
 # Make the docs.
 cd docs
