137fdab63743085dd4af77e578af480f547fae92
anthonyjgatti <anthony.j.gatti@github.com>
2017-11-26 22:51:15 -0500

    Updates to python_runner.sh

--- python_runner.sh
+++ python_runner.sh
@@ -14,7 +14,7 @@ if [ "$2" == "create" ]; then
   cd $PYTHON_PROJECT_DIR
 
   # Clean old environment.
-  if [ "$3" == '-f' ]; then
+  if [ "$3" == "-f" ]; then
     echo "Cleaning..."
     rm -rf ./$1
   else
@@ -35,7 +35,7 @@ if [ "$2" == "create" ]; then
 
   # Add initial files and directories.
   # TODO: Change this we knowonce this works.
-  # mkdir -p ./$1/{$1,docs,resources}
+  # mkdir -p ./$1/{$1,docs,resources,tests}
   # touch ./$1/$1/__init__.py
   cp -r $PYTHON_PROJECT_DIR/lebackup/$1/* .
   touch __status__ # Determines if files have been created.
@@ -43,6 +43,53 @@ if [ "$2" == "create" ]; then
 
   # Add README.
   echo "# $1" >> README.md
+
+  # Add setup.py.
+  cat > setup.py <<EOF
+from setuptools import setup, find_packages, Command
+from setuptools.command.test import test as TestCommand
+
+import logging
+import os
+import pip
+import sys
+logger = logging.getLogger(__name__)
+
+# version - ADD VERSION COMMAND.
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', None, "Arguments to pass to tox")]
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = ''
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        import tox
+        errno = tox.cmdline(args=self.tox_args.split())
+        sys.exit(errno)
+
+def do_setup(install_list):
+    setup(
+        name='$1',
+        license='MIT',
+        version = 0.1,
+        packages=find_packages(exclude=['tests*']),
+        include_package_data=True,
+        install_requires=install_list,
+        cmdclass={
+            'test': Tox
+        }
+    )
+
+if __name__ == '__main__':
+
+    with open('requirements.txt', 'r') as f:
+        install_list = [line for line in f.readlines()]
+    do_setup(install_list)
+EOF
 
   # Add setup.cfg file.
   cat > setup.cfg <<EOF
@@ -71,7 +118,14 @@ include/
 lib/
 lib64
 share/
+*.egg-info/
+pip-selfcheck.json
 # pyvenv.cfg ???
+
+# Python caching.
+__pycache__/
+*.py[cod]
+*$py.class
 
 # vim
 *.swp
@@ -100,6 +154,7 @@ elif [ "$2" == "update" ]; then
   source ./bin/activate
 
   # Install or update local package.
-  pip install $1 --upgrade
+  pip freeze > requirements.txt
+  pip install -e . --upgrade
 
 fi
