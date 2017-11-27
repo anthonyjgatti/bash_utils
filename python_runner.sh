e1a9826877a808e96736fa96447a21e1dd30f13f
anthonyjgatti <anthony.j.gatti@github.com>
2017-11-26 22:08:20 -0500

    Adding python_runner.sh

--- python_runner.sh
+++ python_runner.sh
@@ -2,17 +2,16 @@
 
 # Usage: pybuild atools create -f
 
-# Argument checking goes here.
+# TODO: Add argument checking.
+# TODO: Check for package dependencies: python3, git.
 
 # Create the project.
 if [ "$2" == "create" ]; then
 
-  cd $PYTHON_PROJECT_DIR
+  # TODO: Add setup.py.
+  # TODO: Add Sphinx stuff.
 
-  # TODO: Change this once we know this works.
-  if [ -d "./$1" ]; then
-    cp -r ./$1 $PYTHON_PROJECT_DIR/backup2
-  fi
+  cd $PYTHON_PROJECT_DIR
 
   # Clean old environment.
   if [ "$3" == '-f' ]; then
@@ -32,20 +31,45 @@ if [ "$2" == "create" ]; then
 
   # Create new virtual env.
   python3 -m venv $1
+  cd ./$1
 
-  # TODO: Change this.
-  # mkdir -p ./$1/{$1,resources}
+  # Add initial files and directories.
+  # TODO: Change this we knowonce this works.
+  # mkdir -p ./$1/{$1,docs,resources}
   # touch ./$1/$1/__init__.py
-  # touch __status__
-  cp -r $PYTHON_PROJECT_DIR/lebackup/$1/* ./$1
+  cp -r $PYTHON_PROJECT_DIR/lebackup/$1/* .
+  touch __status__ # Determines if files have been created.
+  touch requirements.txt
+
+  # Add README.
+  echo "# $1" >> README.md
+
+  # Add setup.cfg file.
+  cat > setup.cfg <<EOF
+[metadata]
+name = $1
+description-file = README.md
+license = MIT
+
+[files]
+packages = $1
+
+[build_sphinx]
+source-dir = docs/
+build-dir  = docs/_build
+all_files  = 1
+
+[upload_sphinx]
+upload-dir = docs/_build/html
+EOF
 
   # Write to .gitignore and then git init and commit.
-  cd ./$1
   cat > .gitignore <<EOF
 # Virtual env stuff.
 bin/
 include/
 lib/
+lib64
 share/
 # pyvenv.cfg ???
 
@@ -54,10 +78,28 @@ share/
 
 # OSX
 .DS_Store
+
+# Custom
+__status__
 EOF
 
   git init
   git add -A
   git commit -a -m "Initial commit."
+
+elif [ "$2" == "update" ]; then
+
+  # Check if project has been adequately created.
+  if [ ! -e $PYTHON_PROJECT_DIR/$1/__status__ ]; then
+    echo "[ERROR] Please create package before calling 'update'."
+    exit 1
+  fi
+
+  # Activate virtual environment.
+  cd $PYTHON_PROJECT_DIR/$1/
+  source ./bin/activate
+
+  # Install or update local package.
+  pip install $1 --upgrade
 
 fi
